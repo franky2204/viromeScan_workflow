1e05de71c2e2e7afabc5b90836c71cfdff1db87c
franky2204 <fpantini1997@gmail.com>
2024-07-31 12:00:54 +0200

    eh

--- Viromescan/scripts/removeBacteria.py
+++ Viromescan/scripts/removeBacteria.py
@@ -1,40 +1,32 @@
 import os
 import sys
-
+#open the file conatining the taxids of bacteria and check 
+#if the taxid is in the file
 def IsNotBacteria(taxid):
-    # Open the file taxid.txt in read mode
     with open(sys.argv[2], "r") as taxid_file:
-        # Read the contents of the file
         file_contents = taxid_file.read()
-    
-    # Check if the taxid is present in the file
     if taxid in file_contents:
         return False
     else:
         return True
 
-# Open the file in read mode
+ 
 file_path = sys.argv[1]
 with open(file_path, "r") as file:
-    # Read the contents of the file line by line
     file_contents = file.readlines()
-# Create an empty array to store lines starting with "U"
 lines_starting_with_U = []
 
-# Iterate over the lines in the file
+# If the line starts with "U" or "C" and the taxid is not in the file, 
+# add it to the list
 for line in file_contents:
-    # Check if the line starts with "U"
     if line.startswith("U"):
-        # Add the line to the array
         lines_starting_with_U.append(line)
     elif line.startswith("C"):
-        # Extract the taxid from the current line
         taxid = line.split("\t")[2].split("taxid ")[1].split(")")[0]
         if IsNotBacteria(taxid):
             lines_starting_with_U.append(line)
 
 # Create a new file called "output.txt" in write mode
 with open("notBacteriaLines.txt", "w") as output_file:
-    # Write the lines starting with "U" to the file
     for line in lines_starting_with_U:
         output_file.write(line)
