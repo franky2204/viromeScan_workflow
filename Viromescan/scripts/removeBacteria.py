b76604174fb3164bc5e2e187fbd3a8e369cb8113
franky2204 <fpantini1997@gmail.com>
2024-07-31 14:22:19 +0200

    upgraded remove Bacteria

--- Viromescan/scripts/removeBacteria.py
+++ Viromescan/scripts/removeBacteria.py
@@ -1,32 +1,26 @@
-import os
 import sys
-#open the file conatining the taxids of bacteria and check 
-#if the taxid is in the file
-def IsNotBacteria(taxid):
-    with open(sys.argv[2], "r") as taxid_file:
-        file_contents = taxid_file.read()
-    if taxid in file_contents:
-        return False
-    else:
-        return True
 
- 
+def IsNotBacteria(taxid, taxid_set):
+    return taxid not in taxid_set
+
 file_path = sys.argv[1]
-with open(file_path, "r") as file:
-    file_contents = file.readlines()
+taxid_file_path = sys.argv[2]
+
+
+with open(taxid_file_path, "r") as taxid_file:
+    taxid_set = set(taxid_file.read().splitlines())
+
 lines_starting_with_U = []
 
-# If the line starts with "U" or "C" and the taxid is not in the file, 
-# add it to the list
-for line in file_contents:
-    if line.startswith("U"):
-        lines_starting_with_U.append(line)
-    elif line.startswith("C"):
-        taxid = line.split("\t")[2].split("taxid ")[1].split(")")[0]
-        if IsNotBacteria(taxid):
+with open(file_path, "r") as file:
+    for line in file:
+        if line.startswith("U"):
             lines_starting_with_U.append(line)
+        elif line.startswith("C"):
+            taxid = line.split("\t")[2].split("taxid ")[1].split(")")[0]
+            if IsNotBacteria(taxid, taxid_set):
+                lines_starting_with_U.append(line)
 
-# Create a new file called "output.txt" in write mode
 with open("notBacteriaLines.txt", "w") as output_file:
     for line in lines_starting_with_U:
         output_file.write(line)
